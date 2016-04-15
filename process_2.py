916b37815363ec1e43c6a52e27a230cd63acbd88
Peihao Sun <PeihaoSun@DN51og2j.SUNet>
2016-04-14 22:14:39 -0700

    fully implemented user selection of peaks

--- process_2.py
+++ process_2.py
@@ -4,9 +4,9 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
-from matplotlib.widgets import SpanSelector 
+from matplotlib.widgets import SpanSelector, RectangleSelector 
 from scipy.optimize import curve_fit
-from helpers import findind, get_data, get_pads
+from helpers import findind, get_data, get_pads, get_calib
 
 def fitfunc(x, *params):
 # parameter format:
@@ -31,11 +31,7 @@ def process(pad, run, img, run_bkgd, do_debug, bkgdSubtract):
         imData = imData - imData_bkgd
     
     ## read calibration data
-    params = pickle.load(open("calibration.pickle", 'rb'))[pad]
-    x0 = params['x0']
-    y0 = params['y0']
-    D = params['D']
-    intercept = params['intercept']
+    x0, y0, D, intercept = get_calib(pad)
 
     ## convert to polar coordinates
     # find the extent by looking at the corners
@@ -181,12 +177,11 @@ if __name__=="__main__":
         run_bkgd = 373
         #run_bkgd = 390
     #run_bkgd = 334
-    Npeaks = 2
     do_debug = False 
     bkgdSubtract = True
 
     # parse command line arguments 
-    opts, args = getopt(sys.argv[1:], "p:r:i:b:n:d")
+    opts, args = getopt(sys.argv[1:], "p:r:i:b:d")
     for opt, arg in opts:
         if opt == '-p':
             pad = int(arg)
@@ -199,12 +194,11 @@ if __name__=="__main__":
         elif opt == '-b':
             run_bkgd = int(arg)
             bkgdSubtract = True
-        elif opt == '-n':
-            Npeaks = int(arg)
     
     # process (adjust pad offset)
     imData_polar, rr, tt, twotheta, fr = process(pad, run, img, run_bkgd, \
                                          do_debug, bkgdSubtract)
+    twotheta_deg = twotheta/np.pi*180
     
     # plot data after process
     fig = plt.figure(figsize=(7.5, 15))
@@ -219,80 +213,128 @@ if __name__=="__main__":
     ax2.plot(rr, fr, label='orig.')
 
     if do_debug:
+        ax2.legend()
         plt.show()
         print "Debug mode: exiting before fit"
         sys.exit(0)
 
-    ## fit data
-    if Npeaks == 3:
-        fit_s1 = findind(340, rr)
-        fit_e1 = findind(380, rr)
-        fit_s2 = findind(570, rr)
-        fit_e2 = findind(650, rr)
-    elif Npeaks in [1, 2]:
-        fit_s1 = findind(340, rr)
-        fit_e1 = findind(380, rr)
-        fit_s2 = findind(450, rr)
-        fit_e2 = findind(600, rr)
+    ## let user select range for background fit
+    ## first select fit range
+    fig_fit = plt.figure(figsize=(20,10))
+    ax1_fit = fig_fit.add_subplot(1,1,1)
+    ax1_fit.plot(twotheta_deg, fr)
+    ax1_fit.set_xlabel(r"$2\theta$ (deg)")
+    ax1_fit.set_title("Please select fit range, then close window.")
+    fit_range = [0,0]
+    def onselect1(thmin, thmax):
+        global fit_range
+        indmin, indmax = np.searchsorted(twotheta_deg, (thmin, thmax))
+        indmax = min(len(twotheta_deg)-1, indmax)
+        fit_range = [indmin, indmax]
+
+    span1 = SpanSelector(ax1_fit, onselect1, 'horizontal', useblit=True,\
+                        rectprops=dict(alpha=0.5, facecolor='r'), \
+                        span_stays=True)
+    plt.show()
+
+    ## next select peak range(s) to exclude
+    while True:
+        fig_fit = plt.figure(figsize=(20,10))
+        ax1_fit = fig_fit.add_subplot(1,1,1)
+        ax1_fit.plot(twotheta_deg[fit_range[0]:fit_range[1]], \
+                     fr[fit_range[0]:fit_range[1]])
+        ax1_fit.set_xlabel(r"$2\theta$ (deg)")
+        ax1_fit.set_title("Please select peak range(s) to exclude, then close window.")
+        peaks_range = []
+        def onselect2(thmin, thmax):
+            global fit_range
+            indmin, indmax = np.searchsorted(twotheta_deg, (thmin, thmax))
+            indmax = min(len(twotheta_deg)-1, indmax)
+            peaks_range.append([indmin, indmax])
+            ax1_fit.axvspan(twotheta_deg[peaks_range[-1][0]], twotheta_deg[peaks_range[-1][1]], \
+                            facecolor='g', alpha=0.5)
+
+        span2 = SpanSelector(ax1_fit, onselect2, 'horizontal', useblit=True,\
+                            rectprops=dict(alpha=0.5, facecolor='r'), \
+                            span_stays=False)
+        plt.show()
+        
+        val = raw_input("Continue? (Enter n to redo this step: )")
+        if val not in ['n', 'N']:
+            break 
+
     # first fit background
-    fr_fit = lambda x,a,b,c: a*np.exp(-b*x)+c
-    b0 = -np.log(fr[fit_e2]/fr[fit_s1])/(rr[fit_e2]-rr[fit_s1])
-    a0 = fr[fit_s1]/np.exp(-b0*rr[fit_s1])
+    fit_mask = np.zeros(len(fr))
+    fit_mask[fit_range[0]:fit_range[1]] = 1
+    for i in xrange(len(peaks_range)):
+        fit_mask[peaks_range[i][0]:peaks_range[i][1]] = 0
+    fitmin, fitmax = np.nonzero(fit_mask)[0][0], np.nonzero(fit_mask)[0][-1]
+    bkgd_fit = lambda x,a,b,c: a*np.exp(-b*x)+c
+    b0 = -np.log(fr[fitmin]/fr[fitmax])/(rr[fitmin]-rr[fitmax])
+    a0 = fr[fitmin]/np.exp(-b0*rr[fitmin])
     c0 = 0.
-    popt, pcov = curve_fit(fr_fit, np.concatenate((rr[fit_s1:fit_e1], rr[fit_s2:fit_e2])),\
-                           np.concatenate((fr[fit_s1:fit_e1], fr[fit_s2:fit_e2])), \
-                           p0=[a0, b0, c0])
+    fit_mask = fit_mask.astype(bool)
+    popt, pcov = curve_fit(bkgd_fit, rr[fit_mask], fr[fit_mask], p0=[a0, b0, c0])
     a0, b0, c0 = popt
     print "fit:", popt, np.sqrt(np.diag(pcov))
-    bkgd_start = 6
-    bkgd_end = 500
-    #ax6.plot(rr[bkgd_start:bkgd_end], a0*np.exp(-b0*rr[bkgd_start:bkgd_end])+c0, \
-    #         label='bkgd fit')
-    #ax6.plot(rr[bkgd_start:bkgd_end], (fr-a0*np.exp(-b0*rr)-c0)[bkgd_start:bkgd_end], \
-    #         'r-', label='bkgd fit')
 
-    # now fit peaks + background
-    # setting initial values
-    m1 = 390 # mean
-    m1_ind = findind(m1, rr)
-    s1 = 10 # standard deviation
-    A1 = .5*fr[m1_ind]-a0*np.exp(-b0*rr[m1_ind])-c0 # amplitude
-    m2 = 420 # mean
-    m2_ind = findind(m2, rr)
-    s2 = 10 # standard deviation
-    A2 = .5*fr[m1_ind]-a0*np.exp(-b0*rr[m2_ind])-c0 # amplitude
-    m3 = 520 # mean
-    m3_ind = findind(m3, rr)
-    s3 = 10 # standard deviation
-    A3 = fr[m3_ind]-a0*np.exp(-b0*rr[m3_ind])-c0 # amplitude
-    # see fitfunc for definition of parameter list
-    if Npeaks == 3:
-        params0 = [a0, b0, c0, A1, m1, s1, A2, m2, s2, A3, m3, s3]
-    elif Npeaks == 2: 
-        #params0 = [a0, b0, c0, A1, m1, s1, A2, m2, s2]
-        params0 = [a0, b0, c0, A1, m1, s1, A3, m3, s3]
-    elif Npeaks == 1:
-        params0 = [a0, b0, c0, A1, m1, s1]
-    fit_s = fit_s1 
-    fit_e = fit_e2
-    params, pcov = curve_fit(fitfunc, rr[fit_s:fit_e], fr[fit_s:fit_e], p0=params0)
-    print params
-    ax2.plot(rr[fit_s:fit_e], fitfunc(rr[fit_s:fit_e], *params), label='fit')
+    # then fit peaks + background
+    # Let user choose approximate peak positions (using rectangular regions)
+    while True:
+        params = [a0, b0, c0]
+        fig_fit = plt.figure(figsize=(20,10))
+        ax_fit = fig_fit.add_subplot(1,1,1)
+        ax_fit.plot(twotheta_deg[fitmin:fitmax], fr[fitmin:fitmax], 'b-', \
+                 label='orig.')
+        ax_fit.plot(twotheta_deg[fitmin:fitmax], a0*np.exp(-b0*rr[fitmin:fitmax])+c0, \
+                 'r-', label='bkgd fit')
+        ax_fit.plot(twotheta_deg[fitmin:fitmax], \
+                 fr[fitmin:fitmax]-a0*np.exp(-b0*rr[fitmin:fitmax])+c0, \
+                 'g-', label='minus bkgd')
+        ax_fit.set_xlabel(r"$2\theta$ (deg)")
+        ax_fit.legend()
+        def onselect3(eclick, erelease):
+            x1, y1 = eclick.xdata, eclick.ydata 
+            x2, y2 = erelease.xdata, erelease.ydata 
+            m = 0.5*(x1+x2)
+            s = 0.5*0.5*abs(x2-x1) # this is approximate
+            A = abs(y2-y1)
+            print "mean =", m, "sigma =", s, "amp =", A
+            params.extend([A,m,s])
+            ax_fit.add_patch(Rectangle([min(x1,x2), min(y1,y2)], \
+                                       abs(x2-x1), abs(y2-y1), alpha=0.5,\
+                                       color='g'))
+        rect = RectangleSelector(ax_fit, onselect3, drawtype='box')
+        plt.show()
 
-    ## plot each curve
-    ax2.plot(rr[fit_s:fit_e], \
-             params[0]*np.exp(-params[1]*rr[fit_s:fit_e])+params[2], \
-             label='background')
-            # background
-    for i in range(3, len(params), 3):
-        ax2.plot(rr[fit_s:fit_e], \
-                params[i]*np.exp(-0.5*((rr[fit_s:fit_e]-params[i+1])/params[i+2])**2),
-                label='peak%d' % (i/3)) # peaks
+        try:
+            params, params_cov = curve_fit(fitfunc, twotheta_deg[fitmin:fitmax], \
+                                     fr[fitmin:fitmax], p0=params)
+        except RuntimeError:
+            print "Fit failed! Please choose the peaks again..."
+            continue
 
-    ax2.set_xlabel(r"$r$")
-    ax2.legend()
-    fig.savefig("r%03d_i%01d_rb%03d.pdf" % (run, img, run_bkgd))
-    plt.show()
+        # show the fit functions
+        plt.plot(twotheta_deg[fitmin:fitmax], fr[fitmin:fitmax], \
+                 label='orig.')
+        plt.plot(twotheta_deg[fitmin:fitmax], \
+                 fitfunc(twotheta_deg[fitmin:fitmax], *params), \
+                 label='fit')
+        plt.plot(twotheta_deg[fitmin:fitmax], \
+                 params[0]*np.exp(-params[1]*twotheta_deg[fitmin:fitmax])+params[2], \
+                 label='bkgd')
+        for i in range(3, len(params), 3):
+            plt.plot(twotheta_deg[fitmin:fitmax], \
+                    params[i]*np.exp(-0.5*((twotheta_deg[fitmin:fitmax]-params[i+1])/params[i+2])**2),
+                    label='peak%d' % (i/3))
+
+        plt.xlabel(r"$2\theta$ (deg)")
+        plt.legend()
+        plt.show()
+
+        val = raw_input("Continue? Enter n to redo this step: ")
+        if val not in ['n', 'N']:
+            break
 
     # save data
     do_save = raw_input("Save data? ")
@@ -303,7 +345,8 @@ if __name__=="__main__":
         data = pickle.load(open("data_2.pickle", 'rb'))
         thisrun = {'img': img, 'run_bkgd': run_bkgd, 'twotheta': twotheta,\
                    'fr': fr, 'bkgdSubtract': bkgdSubtract}
-        thisrun['Npeaks'] = Npeaks
+        thisrun['Npeaks'] = len(params)/3 - 1
         thisrun['params'] = params
+        thisrun['params_err'] = np.sqrt(np.diag(params_cov))
         data[run] = thisrun
         pickle.dump(data, open("data_2.pickle", 'wb'))
