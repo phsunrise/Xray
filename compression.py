c74df5a552fae7f2962f2a7fe4ef1101932399cd
phsunrise <phsun@stanford.edu>
2016-04-21 10:55:12 -0700

    now running over both apds

--- compression.py
+++ compression.py
@@ -5,6 +5,7 @@ import pickle
 import matplotlib.pyplot as plt
 
 coldpeaks = np.array([37.3, 43.3])
+coldpeaks_label = ['(111)', '(200)']
 ratio = np.linspace(1., 3., 200)
 comppeaks = []
 for i in xrange(len(coldpeaks)):
@@ -12,30 +13,49 @@ for i in xrange(len(coldpeaks)):
                               * (ratio**(1./3)) )
                     /np.pi*180.)
 
-opts, args = getopt(sys.argv[1:], "p:r:")
-for opt, arg in opts:
-    if opt == '-p':
-        pad = int(arg)
-    #elif opt == '-r':
-    #    run = int(arg)
+#opts, args = getopt(sys.argv[1:], "p:")
+#for opt, arg in opts:
+#    if opt == '-p':
+#        pad = int(arg)
 
-data = pickle.load(open("data_%d.pickle" % pad, 'rb'))
-for run in data.keys():
+data0 = pickle.load(open("data_0.pickle", 'rb'))
+data2 = pickle.load(open("data_2.pickle", 'rb'))
+for run in data2.keys():
     try:
         run = int(run)
     except ValueError:
         continue
 
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1)
     # plot measured peaks
-    params = data[run]['params']
-    for i in range(3, len(params), 3):
-        plt.axhline(y=params[i+1], c='k', ls='-')
+    params2 = data2[run]['params']
+    for i in range(3, len(params2), 3):
+        ax1.axhline(y=params2[i+1], c='k', ls='-')
+
+    try:
+        params0 = data0[run]['params']
+        for i in range(3, len(params0), 3):
+            ax1.axhline(y=params0[i+1], c='b', ls='-')
+    except KeyError:
+        pass
+
+    ax1.set_xlabel("compression ratio")
+    ax1.set_ylabel(r"$2\theta$ (deg)", color='k')
 
     # plot compressed cold peaks
     for i in xrange(len(coldpeaks)):
-        plt.plot(ratio, comppeaks[i], 'r-')
+        ax1.plot(ratio, comppeaks[i], 'r-')
 
-    plt.xlabel("compression ratio")
-    plt.ylabel(r"$2\theta$ (deg)")
-    plt.title("Shot %d" % run)
-    plt.savefig("r%04d_p%d_compression.pdf" % (run, pad))
+    # add labels on right 
+    ax2 = ax1.twinx()
+    xmin, xmax = ax1.get_xlim()
+    ymin, ymax = ax1.get_ylim()
+    ax2.set_ylim(ymin, ymax)
+    ax2.set_yticks(np.arctan(np.tan(coldpeaks/180.*np.pi)
+                  * (xmax**(1./3))) / np.pi * 180.)
+    ax2.set_yticklabels(coldpeaks_label, color='r')
+
+    ax1.set_title("Shot %d" % run)
+    fig.savefig("r%04d_compression.pdf" % run)
+    plt.close()
